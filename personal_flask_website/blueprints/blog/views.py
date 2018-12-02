efc8e8082f28bcae18795140e8aff606379a96e3
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2018-12-02 00:06:10 +0000

    Added during blog stuff, aboutme stuff, and update gitignore to ignore vendor bootstrap files

---
+++ personal_flask_website/blueprints/blog/views.py
@@ -0,0 +1,8 @@
+from flask import (Blueprint, render_template)
+
+blog = Blueprint('blog', __name__, template_folder='templates')
+
+@blog.route('/blog')
+def index():
+    return render_template('blog/index.html')
+
