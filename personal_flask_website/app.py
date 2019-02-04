8db20bf476389c57a5bc45803cec7b4844cce24c
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-02-04 00:55:55 +0000

    added files and directories needed for making yt2spec forms

--- personal_flask_website/app.py
+++ personal_flask_website/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from personal_flask_website.blueprints.page import page
 from personal_flask_website.blueprints.blog import blog
 
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(page)
