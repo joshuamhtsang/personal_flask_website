2f591ccce752ecc03a9e16d8e71fee459f6e9e27
Man Hong Tsang <joshua.mh.tsang@gmail.com>
2019-02-06 17:15:47 +0000

    Finally got redirect to work with form submission

--- personal_flask_website/blueprints/page/views.py
+++ personal_flask_website/blueprints/page/views.py
@@ -7,9 +7,11 @@ page = Blueprint('page', __name__, template_folder='templates')
 def home():
     return render_template('page/home.html')
 
+
 @page.route('/cv')
 def cv():
     return render_template('page/cv.html')
+
 
 @page.route('/aboutme')
 def aboutme():
--- personal_flask_website/blueprints/yt2spec/views.py
+++ personal_flask_website/blueprints/yt2spec/views.py
@@ -1,10 +1,13 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, flash, redirect, url_for)
 from personal_flask_website.blueprints.yt2spec.forms import SpecForm
 
 yt2spec = Blueprint('yt2spec', __name__, template_folder='templates')
 
 
-@yt2spec.route('/yt2spec')
+@yt2spec.route('/yt2spec', methods=['GET', 'POST'])
 def index():
     form = SpecForm()
+    if form.validate_on_submit():
+        flash("helpful message here")
+        return redirect(url_for('blog.index'))
     return render_template('yt2spec/index.html', title='Placeholder', form=form)
