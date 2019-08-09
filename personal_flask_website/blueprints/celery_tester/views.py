d2b4abe801378db729c33eb1dfb3db1164acf83e
joshuamhtsang <joshua.mh.tsang@gmail.com>
2019-08-09 18:39:46 +0100

    Got sleeper working without celery workers yet

--- personal_flask_website/blueprints/celery_tester/views.py
+++ personal_flask_website/blueprints/celery_tester/views.py
@@ -13,6 +13,18 @@ celery_tester = Blueprint('celery_tester', __name__, template_folder='templates'
 @celery_tester.route('/celerytester', methods=['GET', 'POST'])
 def index():
     form = SleeperForm()
+    if form.validate_on_submit():
+        flash('Sleep duration: %s' % form.sleep_duration.data)
+
+        sleeper_url = 'http://sleeper:1234'.rstrip()
+        endpoint_name = 'look_busy'.rstrip()
+        sleeper_ep_url = sleeper_url + '/' + endpoint_name
+        sleep_duration = form.sleep_duration.data
+        payload = {
+            'n': sleep_duration
+        }
+        response = requests.get(sleeper_ep_url, params=payload)
+        print(response)
     return render_template('celery_tester/index.html', title='Placeholder', form=form)
 
 
