2d279c1fb95bca4b71f05df3c79a7592b053d538
joshuamhtsang <joshua.mh.tsang@gmail.com>
2019-11-10 23:08:33 +0000

    Added form

--- personal_flask_website/blueprints/short_straw/__init__.py
+++ personal_flask_website/blueprints/short_straw/__init__.py
@@ -1,1 +1,2 @@
 from blueprints.short_straw.views import short_straw
+from blueprints.short_straw.forms import ListForm
--- personal_flask_website/blueprints/short_straw/views.py
+++ personal_flask_website/blueprints/short_straw/views.py
@@ -1,8 +1,12 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, flash)
+from blueprints.short_straw.forms import ListForm
 
 short_straw = Blueprint('short_straw', __name__, template_folder='templates')
 
 
 @short_straw.route('/shortstraw', methods=['GET', 'POST'])
 def index():
+    form = ListForm()
+    if form.validate_on_submit():
+        flash('List submitted: %s' % form.choices_list_str)
     return render_template('short_straw/index.html', title='Placeholder')
