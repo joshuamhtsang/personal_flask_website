49ab96a4e3497eeabd17e3838839bc367dbeab71
joshuamhtsang <joshua.mh.tsang@gmail.com>
2020-02-18 15:39:17 +0000

    ui now adds new choices list by making the relevant api call

--- personal_flask_website/blueprints/short_straw/views.py
+++ personal_flask_website/blueprints/short_straw/views.py
@@ -1,12 +1,37 @@
 from flask import (Blueprint, render_template, flash)
-from blueprints.short_straw.forms import ListForm
+from blueprints.short_straw.forms import ListForm, RandomDrawForm
+import requests
+import json
 
 short_straw = Blueprint('short_straw', __name__, template_folder='templates')
 
 
 @short_straw.route('/shortstraw', methods=['GET', 'POST'])
 def index():
-    form = ListForm()
-    if form.validate_on_submit():
-        flash('List submitted: %s' % form.choices_list_str)
-    return render_template('short_straw/index.html', title='Placeholder', form=form)
+    # ListForm()
+    list_form = ListForm()
+    if list_form.validate_on_submit():
+        flash('List submitted: %s' % list_form.choices_list_str.data)
+
+        # Make request to short_straw API.
+        url = 'http://shortstraw:2828/sessions'
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        data = {
+            "name": "whatever",
+            "choices": list_form.choices_list_str.data,
+        }
+        try:
+            response = requests.post(url, headers=headers, data=json.dumps(data))
+            response.raise_for_status()
+        except Exception as e:
+            return data
+
+    # RandomDrawForm()
+    draw_form = RandomDrawForm()
+    if draw_form.validate_on_submit():
+        flash('Draw from session: %s' % draw_form.session_id_str)
+
+    return render_template('short_straw/index.html', title='Placeholder',
+                           list_form=list_form, draw_form=draw_form)
